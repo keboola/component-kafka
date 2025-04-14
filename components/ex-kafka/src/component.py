976cc1b96835a7f88ecdcd6669767c94141e8cd3
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-04-14 13:05:17 +0200

    ruff, new datadirtest version

--- components/ex-kafka/src/component.py
+++ components/ex-kafka/src/component.py
@@ -3,31 +3,29 @@ Template Component main class.
 
 """
 
-import logging
-import os
 import csv
 import json
+import logging
+import os
 from collections import OrderedDict
-from typing import TextIO
-import polars
 from dataclasses import dataclass
-from keboola.component.base import ComponentBase, sync_action
-from keboola.component.sync_actions import SelectElement, ValidationResult, MessageType
-from keboola.component.exceptions import UserException
-from keboola.component.dao import ColumnDefinition, BaseType
+from typing import TextIO
 
-from configuration import Configuration
-
+import polars
 from common.src.kafka_client import KafkaConsumer
-
+from configuration import Configuration
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroDeserializer
-from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.serialization import MessageField, SerializationContext
+from keboola.component.base import ComponentBase, sync_action
+from keboola.component.dao import BaseType, ColumnDefinition
+from keboola.component.exceptions import UserException
+from keboola.component.sync_actions import MessageType, SelectElement, ValidationResult
 
 # global constants
-RESULT_PK = ['key', 'offset']
-RESULT_COLS = ['topic', 'timestamp_type', 'timestamp', 'partition', 'offset', 'key', 'value']
-RESULT_COLS_DTYPES = ['string', 'string', 'timestamp', 'int', 'int', 'string', 'string']
+RESULT_PK = ["key", "offset"]
+RESULT_COLS = ["topic", "timestamp_type", "timestamp", "partition", "offset", "key", "value"]
+RESULT_COLS_DTYPES = ["string", "string", "timestamp", "int", "int", "string", "string"]
 
 
 @dataclass
@@ -37,7 +35,6 @@ class CachedWriter:
 
 
 class Component(ComponentBase):
-
     def __init__(self):
         self.params = None
         self.client = None
@@ -62,9 +59,10 @@ class Component(ComponentBase):
         self.params = Configuration(**self.configuration.parameters)
         self._validate_stack_params()
 
-        self.params.group_id = (f"kbc-proj-{self.environment_variables.project_id or "local"}-"
-                                f"{self.environment_variables.config_row_id}")
-        self.params.client_id = f"kbc-config-{self.environment_variables.config_row_id or "local"}"
+        self.params.group_id = (
+            f"kbc-proj-{self.environment_variables.project_id or 'local'}-{self.environment_variables.config_row_id}"
+        )
+        self.params.client_id = f"kbc-config-{self.environment_variables.config_row_id or 'local'}"
 
         # Generating a string out of the list
         bootstrap_servers = ",".join(self.params.bootstrap_servers)
@@ -78,7 +76,7 @@ class Component(ComponentBase):
 
         for topic in self.params.topics:
             msg_cnt, res_file_folder, schema = self.consume_topic(topic)
-            self.topics[topic] = {'msg_cnt': msg_cnt, 'res_file_folder': res_file_folder, 'schema': schema}
+            self.topics[topic] = {"msg_cnt": msg_cnt, "res_file_folder": res_file_folder, "schema": schema}
 
         self.close_all_writers()
 
@@ -92,7 +90,7 @@ class Component(ComponentBase):
 
     def _validate_stack_params(self):
         image_parameters = self.configuration.image_parameters or {}
-        allowed_hosts = [f"{host.get('host')}:{host.get('port')}" for host in image_parameters.get('allowed_hosts', [])]
+        allowed_hosts = [f"{host.get('host')}:{host.get('port')}" for host in image_parameters.get("allowed_hosts", [])]
 
         if allowed_hosts:
             for item in self.params.bootstrap_servers:
@@ -101,30 +99,28 @@ class Component(ComponentBase):
 
     def produce_manifest(self):
         for topic, consumed in self.topics.items():
-
             schema = OrderedDict()
             for col, dtype in zip(RESULT_COLS, RESULT_COLS_DTYPES):
                 schema[col] = ColumnDefinition(data_types=self.convert_dtypes(dtype))
 
-            if consumed.get('schema'):
-                del schema['value']
+            if consumed.get("schema"):
+                del schema["value"]
 
-            for col in consumed.get('schema'):
-                schema[col.get('name')] = ColumnDefinition(data_types=self.convert_dtypes(col.get('type')))
+            for col in consumed.get("schema"):
+                schema[col.get("name")] = ColumnDefinition(data_types=self.convert_dtypes(col.get("type")))
 
             # Produce final sliced table manifest
-            if consumed['msg_cnt'] > 0:
-                logging.info(F'Fetched {consumed['msg_cnt']} messages from topic - {topic}')
-                out_table = self.create_out_table_definition(consumed['res_file_folder'], is_sliced=True,
-                                                             primary_key=RESULT_PK, schema=schema,
-                                                             incremental=True)
+            if consumed["msg_cnt"] > 0:
+                logging.info(f"Fetched {consumed['msg_cnt']} messages from topic - {topic}")
+                out_table = self.create_out_table_definition(
+                    consumed["res_file_folder"], is_sliced=True, primary_key=RESULT_PK, schema=schema, incremental=True
+                )
 
                 self.write_manifest(out_table)
             else:
-                logging.info('No new messages found!')
+                logging.info("No new messages found!")
 
     def consume_topic(self, topic):
-
         self.columns.setdefault(topic, RESULT_COLS)
 
         deserializer = self.get_deserializer()
@@ -142,12 +138,12 @@ class Component(ComponentBase):
 
             extracted_data, last_message = self.get_message_data(deserializer, last_message, msg, topic)
 
-            filename = (("p{0}-{1}.csv").format(
+            filename = ("p{0}-{1}.csv").format(
                 msg.partition(),
                 msg.offset() // 10_000,
-            ))
+            )
 
-            logging.debug(F'Received message: {extracted_data}')
+            logging.debug(f"Received message: {extracted_data}")
 
             # Save data as a sliced table file in defined folder
             self.save_file(extracted_data, os.path.join(res_file_folder, filename), topic)
@@ -158,43 +154,44 @@ class Component(ComponentBase):
             if msg.topic() not in self.latest_offsets:
                 self.latest_offsets[msg.topic()] = {}
 
-            self.latest_offsets[msg.topic()]['p' + str(msg.partition())] = msg.offset()
+            self.latest_offsets[msg.topic()]["p" + str(msg.partition())] = msg.offset()
 
-        if self.params.deserialize == 'avro' and self.params.flatten_message_value_columns:
+        if self.params.deserialize == "avro" and self.params.flatten_message_value_columns:
             dtypes = self.get_topic_dtypes(last_message)
 
         return msg_cnt, res_file_folder, dtypes
 
     def get_message_data(self, deserializer, last_message, msg, topic):
-        if self.params.deserialize == 'avro':
+        if self.params.deserialize == "avro":
             value = deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
         else:
-            value = msg.value().decode('utf-8')
+            value = msg.value().decode("utf-8")
         if self.params.freeze_timestamp:  # freeze for datadir tests
             timestamp = 1732104020556
         else:
             timestamp = msg.timestamp()[1]
         extracted_data = {
-            'topic': msg.topic(),
-            'timestamp_type': msg.timestamp()[0],
-            'timestamp': timestamp,
-            'partition': msg.partition(),
-            'offset': msg.offset(),
-            'key': msg.key()}
+            "topic": msg.topic(),
+            "timestamp_type": msg.timestamp()[0],
+            "timestamp": timestamp,
+            "partition": msg.partition(),
+            "offset": msg.offset(),
+            "key": msg.key(),
+        }
         if self.params.flatten_message_value_columns:
             self.safe_update(extracted_data, value)
             self.columns[topic] = list(extracted_data.keys())
             last_message = msg.value()  # to get dtypes
         else:
-            extracted_data['value'] = value
+            extracted_data["value"] = value
         return extracted_data, last_message
 
     def get_deserializer(self):
         deserializer = None
-        if self.params.deserialize == 'avro':
+        if self.params.deserialize == "avro":
             if self.params.schema_registry_url:
                 config = self.params.schema_registry_extra_params
-                config['url'] = self.params.schema_registry_url
+                config["url"] = self.params.schema_registry_url
                 schema_registry_client = SchemaRegistryClient(config)
                 deserializer = AvroDeserializer(schema_registry_client)
             elif self.params.schema_str:
@@ -205,28 +202,28 @@ class Component(ComponentBase):
 
     def get_topic_dtypes(self, message_value: str):
         schema = None
-        if self.params.deserialize == 'avro':
+        if self.params.deserialize == "avro":
             if self.params.schema_registry_url:
                 config = self.params.schema_registry_extra_params
-                config['url'] = self.params.schema_registry_url
+                config["url"] = self.params.schema_registry_url
                 schema_registry_client = SchemaRegistryClient(config)
                 schema_id = int.from_bytes(message_value[1:5])
-                schema = json.loads(schema_registry_client.get_schema(schema_id).schema_str).get('fields')
+                schema = json.loads(schema_registry_client.get_schema(schema_id).schema_str).get("fields")
 
             elif self.params.schema_str:
-                schema = json.loads(self.params.schema_str).get('fields')
+                schema = json.loads(self.params.schema_str).get("fields")
 
         return schema
 
-    def convert_dtypes(self, dtype: str = 'string'):
+    def convert_dtypes(self, dtype: str = "string"):
         match dtype:
-            case 'boolean':
+            case "boolean":
                 base_type = BaseType.boolean()
-            case 'int':
+            case "int":
                 base_type = BaseType.integer()
-            case 'float':
+            case "float":
                 base_type = BaseType.float()
-            case 'double':
+            case "double":
                 base_type = BaseType.float()
             case _:
                 base_type = BaseType.string()
@@ -234,20 +231,22 @@ class Component(ComponentBase):
         return base_type
 
     def _init_client(self, debug, params, prev_offsets, bootstrap_servers):
-        c = KafkaConsumer(bootstrap_servers=bootstrap_servers,
-                          group_id=params.group_id,
-                          client_id=params.client_id,
-                          security_protocol=params.security_protocol,
-                          sasl_mechanisms=params.sasl_mechanisms,
-                          username=params.username,
-                          password=params.password,
-                          ssl_ca=params.ssl_ca,
-                          ssl_key=params.ssl_key,
-                          ssl_certificate=params.ssl_certificate,
-                          logger=logging.getLogger(),
-                          start_offset=prev_offsets,
-                          config_params=params.kafka_extra_params,
-                          debug=debug)
+        c = KafkaConsumer(
+            bootstrap_servers=bootstrap_servers,
+            group_id=params.group_id,
+            client_id=params.client_id,
+            security_protocol=params.security_protocol,
+            sasl_mechanisms=params.sasl_mechanisms,
+            username=params.username,
+            password=params.password,
+            ssl_ca=params.ssl_ca,
+            ssl_key=params.ssl_key,
+            ssl_certificate=params.ssl_certificate,
+            logger=logging.getLogger(),
+            start_offset=prev_offsets,
+            config_params=params.kafka_extra_params,
+            debug=debug,
+        )
         return c
 
     def safe_update(self, extracted_data, value):
@@ -261,13 +260,13 @@ class Component(ComponentBase):
         """
         Save text as file
         """
-        logging.info(F'Writing file {filename}')
+        logging.info(f"Writing file {filename}")
 
         if filename not in self.writers:
             if not os.path.exists(os.path.dirname(filename)):
                 os.makedirs(os.path.dirname(filename))
 
-            file = open(filename, 'a')
+            file = open(filename, "a")
             writer = csv.DictWriter(file, fieldnames=self.columns[topic])
             self.writers[filename] = CachedWriter(file=file, writer=writer)
             logging.info(f"Created new writer for {filename}")
@@ -309,7 +308,7 @@ class Component(ComponentBase):
 
             polars.Config.set_tbl_formatting("ASCII_MARKDOWN")
             polars.Config.set_tbl_hide_dataframe_shape(True)
-            df = polars.DataFrame(extracted_data.get('value'))
+            df = polars.DataFrame(extracted_data.get("value"))
             md_table_output = str(df)
 
             return ValidationResult(md_table_output, MessageType.SUCCESS)
